bfb7122d9c0aadcc5943f6c2a052adbd83d7cdd8
Salih Saygi <salih.saygie@gmail.com>
2021-05-09 12:15:08 -0700

    rerouting for file directory changes

--- website/frontend/modules/dashboard/adminDash/Main.js
+++ website/frontend/modules/dashboard/adminDash/Main.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Navbar from './Navbar'
-import Bots from '../../data/bot/botList'
-import Users from '../../data/user/userList';
+import Bots from '../../data/back/bot/botList'
+import Users from '../../data/back/user/userList';
 import { ReactQueryDevtools } from 'react-query-devtools';
 
 
