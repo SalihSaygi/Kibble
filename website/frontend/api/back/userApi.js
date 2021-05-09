279ab6b05bf082dc001d54c6339ea346a5292f04
Salih Saygi <salih.saygie@gmail.com>
2021-05-09 12:12:58 -0700

    moved files

---
+++ website/frontend/api/back/userApi.js
@@ -0,0 +1,59 @@
+import axios from 'axios'
+
+async function getUsers() {
+    try {
+        const { data } = await axios.get('/search')
+        return data
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function getUser({ queryKey }) {
+    const [_key, { id }] = queryKey
+    try {
+        const { data } = await axios.get(`/api/users/${id}`)
+        return data
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+async function getProfile() {
+    try{
+        const { data } = await axios.get('/api/users/profile')
+        return data
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+async function postApiToken(apiToken) {
+    try{
+        const { status } = await axios.put('/api/users/profile', apiToken)
+        return status
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+async function getProfileBots() {
+    try{
+        const { data } = await axios.get('/api/users/profile/bots')
+        return data
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+async function getProfileOneBot(id) {
+    try{
+        const { data } = await axios.get(`/api/users/profile/bots/${id}`)
+        return data
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+
+export { getUsers, getUser, getProfile, postApiToken, getProfileBots, getProfileOneBot }
