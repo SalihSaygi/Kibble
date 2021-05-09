f2b72d2429575a3002137694590aff009c0ac4f2
Salih Saygi <salih.saygie@gmail.com>
2021-05-09 12:03:29 -0700

    forgot password fix

--- website/back/controllers/passwordController.js
+++ website/back/controllers/passwordController.js
@@ -1,62 +1,72 @@
-import crypto from 'crypto'
 import Privileged from '../models/privilegedModel.js';
+import {PasswordReset} from '../models/resetModel.js'
 import passportLocalMongoose  from 'passport-local-mongoose'
+import { sendEmail } from '../Gmail.js'
+import { resetPassword } from '../helper/resetPass.js'
+
 
 const forgot = async(req, res) => {
-    const token = (await promisify(crypto.randomBytes)(20)).toString('hex');
-    const user = Privileged.find(user => user.email === req.body.email);
-
-    if (!user) {
-        return
-        // return res.redirect('/forgot');
-    }
-
-    user.resetPasswordToken = token;
-    user.resetPasswordExpires = Date.now() + 1000 * 60 * 60; //1 hour
-
-        sendEmail({
-        to: user.email,
-        from: 'KibbleBot1337@gmail.com',
-        subject: 'KibbleBot Password Reset',
-        text: `
-        Click this to reset your password
-        http://${req.headers.host}/reset/${token}
-        If you did not request this, please ignore this email and your password will remain unchanged.`})
+    
+    const { email } = req.body
+    console.log(email)
+    const user = Privileged.findOne({ email })
+            if (!user) {
+                res.status(400)
+                console.log("No user with that email")
+                throw new Error("No user with that email")
+                return
+            }
+            let token
+            try {
+                token = await PasswordReset.plaintextToken()
+            } catch {
+                  console.log('That did not go well.')
+            }
+            console.log(token)
+            let reset
+            try {
+                reset = await PasswordReset.create({ userId: user.id, token })
+            } catch {
+                console.log('That also did not go well.')
+            }
+            console.log(reset)
+            const fromEmail = process.env.EMAIL
+            console.log(fromEmail)
+            sendEmail({
+            from: fromEmail,
+            to: email,
+            subject: 'KibbleBot Password Reset',
+            text: reset.url(token)
+            })
+            res.status(201)
 }
 
-const getToken = (req, res) => {
-    const user = Privileged.find(u => (
-        (u.resetPasswordExpires > Date.now()) &&
-        crypto.timingSafeEqual(Buffer.from(u.resetPasswordToken), Buffer.from(req.params.token))
-    ));
+const passwordReset = async({ query, body }, res) => {
+  const { id, token } = query
+  const { password } = body
+  console.log(password)
+  const reset = await PasswordReset.findById(id)
+  console.log(reset)
+  let user
 
-    if (!user) {
-        return
-        // return res.redirect('/forgot');
-    }
-}
-
-const postToken = (req, res) => {
-    const user = Privileged.find(u => (
-    (u.resetPasswordExpires > Date.now()) &&
-    crypto.timingSafeEqual(Buffer.from(u.resetPasswordToken), Buffer.from(req.params.token))
-  ));
-
-  if (!user) {
-      return
-    // return res.redirect('/forgot');
+  if (!reset) {
+    throw new Error('Invalid password reset token')
   }
 
-  user.password = req.body.password;
-  delete user.resetPasswordToken;
-  delete user.resetPasswordExpires;
+  await Promise.all([
+    resetPassword(user, password),
+    PasswordReset.deleteMany({ userId: reset.userId })
+  ])
+const fromEmail = process.env.EMAIL
 
-    sendEmail({
-        to: user.email,
-        from: 'kibblebot1337@gmail.com',
-        subject: 'Your password has been changed',
-        text: `This is a confirmation that the password for your account "${user.email}" has just been changed.`
-    })
+  await sendMail({
+    from: fromEmail,
+    to: user.email,
+    subject: 'Password reset',
+    text: 'Your password was successfully reset'
+  })
+
+  res.json({ message: 'OK' })
 }
 
 const changePass = (req, res) => {
@@ -85,4 +95,4 @@ const changePass = (req, res) => {
     })
 }
 
-export { forgot, getToken, postToken, changePass }
+export { forgot, passwordReset, changePass }
