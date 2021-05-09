81274dcf1f7ef393412e329c746d89d03f264443
Salih Saygi <salih.saygie@gmail.com>
2021-05-09 12:00:59 -0700

    remove unneccesary typescipt defaults for env variables

--- website/back/config/app.js
+++ website/back/config/app.js
@@ -1,11 +1,9 @@
 export const {
-  NODE_ENV = 'development',
-
-  APP_PORT = 3000,
-  APP_HOSTNAME = 'localhost',
-  APP_PROTOCOL = 'http',
-
-  APP_SECRET = '4d2ca599b4189f74a771f44b8a8d06f572208b5649f5ae216f8e94612a267ff0'
+  NODE_ENV,
+  APP_PORT,
+  APP_HOSTNAME,
+  APP_PROTOCOL,
+  APP_SECRET
 } = process.env
 
 export const APP_ORIGIN = `${APP_PROTOCOL}://${APP_HOSTNAME}:${APP_PORT}`
